75b95e7de1ff978315992834e4fbd398548e3d99
alisena <alisenamudaber@gmail.com>
2019-04-06 16:48:27 +0100

    Updates

---
+++ precache-manifest.6c9e508a28ef3967a4a094b610cce3ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "233277cbcee9131d0831",
+    "url": "/youtube-api/static/css/main.18936acd.chunk.css"
+  },
+  {
+    "revision": "233277cbcee9131d0831",
+    "url": "/youtube-api/static/js/main.f00dfda5.chunk.js"
+  },
+  {
+    "revision": "005f278b5305a7aab350",
+    "url": "/youtube-api/static/js/runtime~main.9f8da7b4.js"
+  },
+  {
+    "revision": "8b87138b5a7c4e0d705a",
+    "url": "/youtube-api/static/js/2.518cda0c.chunk.js"
+  },
+  {
+    "revision": "619b7a3390e43bfe1ac7815a29b71e50",
+    "url": "/youtube-api/index.html"
+  }
+];
